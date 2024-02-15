2453ea4f2c29909865158c6a8af11273164139f8
masksure2402v <156318777+masksure2402v@users.noreply.github.com>
2024-02-16 05:00:00 +0530

    Add files via upload

---
+++ j.js
@@ -0,0 +1,121 @@
+//var masage ="Hello";
+//var name = "ujjwal";
+//alert(masage+name);
+
+
+// var twitte = prompt("write your twitte");
+// var twittecount =twitte.length;
+// alert ("you have remmaing "+ (100-twittecount) +" chareters");
+
+// var twitte = prompt("write your twitte");
+// twitte.slice=0,140,alert ("limit reached");
+
+// var name = prompt("write your name");
+// var firstchar = name.slice(0,1);
+// var ucfistchar = firstchar.toUpperCase();
+// var restOfname = name.slice(1,name.lenght);
+// alert("Hello "+ucfistchar + restOfname);
+
+
+
+// dog to human age calulater
+
+// var dogage = prompt("enter your dog's age");
+// humanage = (dogage-2)*4+21;
+// alert("Your dog's human age is "+humanage);
+
+
+
+// giving x money and asking how many bottal I can get if the prise is 2ruppes.
+
+// alert(getMilk(prompt()));
+// function getMilk (bottal){
+//     alert("go to the shop ");
+//     alert("buy "+(bottal/2)+" bottals of milk");
+// }
+
+
+
+
+// this code will tell you. your years left to live. 
+
+// function timeleft (){
+//     var yourage=prompt("Enter your age.");
+//     var limitY = 80;
+//     var year = limitY-yourage;
+//     var month = year*12;
+//     var week = year*52;
+//     var days = year*365;
+//     alert("you have "+year+" years, "+month+" months, "+week+" weeks, "+days+" days Left to live.")
+// }
+// timeleft()
+
+
+// love persentage calculator
+// prompt("what's your name ");
+// prompt("what's your name ");
+// var lovescore = Math.floor(Math.random() * 100+1);
+
+// if(lovescore > 70){alert ("Love Score = "+lovescore+ "%  You are soulmates.")}
+
+// if (lovescore < 70 && lovescore > 30){alert("Love Score = "+lovescore+ "%  You like him/her")}
+
+// if (lovescore <= 30){alert("Love Score = "+lovescore+ "%  You really love him/her?")}
+
+
+
+
+//=>(BMI) body mass index calulater.
+
+// var height = prompt("height");
+// var weight = prompt("weight");
+// var bmi = weight / (height*height);
+
+// if (bmi < 18.5){alert("Your BMI is "+Math.round(bmi)+", so you are underweight.")}
+
+// if (bmi > 18.5 && bmi < 24.9){alert("Your BMI is "+Math.round(bmi)+", so you have a normal weight.")}
+
+// if (bmi > 24.9){alert ("Your BMI is "+Math.round(bmi)+", so you are overweight.")}
+
+
+
+
+
+
+
+
+// code fizzbuzz Question 
+
+// var output = [];
+// var x = 1
+// function fizzbuzz (){
+//     if(x % 3 === 0 && x % 5 === 0 ){
+//         output.push("fizzbuzz")
+//     }else if (x % 3 === 0) {
+//         output.push("fizz")
+//     } else if (x % 5 === 0 ) {
+//         output.push("buzz")
+//     }else{
+//         output.push(x)
+//     }
+//     x++
+    
+
+//     console.log(output)
+// }
+
+
+function whosPaying(names) {
+    
+    /******Don't change the code above*******/
+        
+        //Write your code here.
+        Math.random(names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]);
+        alert(names)
+    
+        
+        
+    
+    
+    /******Don't change the code below*******/    
+    }
